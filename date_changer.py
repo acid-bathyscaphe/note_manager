0ce81c390289e62a8c7f2cb3b499f7d4954fc651
acid-bathyscaphe <Sisterovandrej314@gmail.com>
2024-11-25 20:13:19 +0500

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,12 @@
+created_day = 25
+created_month = 11
+created_year = 2024
+issue_day = 1
+issue_month = 12
+issue_year = 2024
+created_date = str(created_day) + '.' + str(created_month) + '.' + str(created_year)
+issue_date = str(issue_day) + '.' + str(issue_month) + '.' + str(issue_year)
+temp_created_date = str(created_day) + '.' + str(created_month)
+temp_issue_date = str(issue_day) + '.' + str(issue_month)
+print(temp_created_date)
+print(temp_issue_date)
