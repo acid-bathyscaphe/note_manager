c97f58890cfd2bb8b5ec8160529cea620d2fd99a
acid-bathyscaphe <Sisterovandrej314@gmail.com>
2024-11-25 19:42:00 +0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Андрей"
+title = "Важные дела"
+content = "Список важных дел на текущую неделю"
+status = "В процессе"
+created_date = "25.11.2024"
+issue_date = "1.12.2024"
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
