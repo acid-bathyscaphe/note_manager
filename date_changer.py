2c7f2503927dac399bb032d445139b8aa441ec0f
acid-bathyscaphe <Sisterovandrej314@gmail.com>
2024-11-25 20:41:11 +0500

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,12 +1,4 @@
-created_day = 25
-created_month = 11
-created_year = 2024
-issue_day = 1
-issue_month = 12
-issue_year = 2024
-created_date = str(created_day) + '.' + str(created_month) + '.' + str(created_year)
-issue_date = str(issue_day) + '.' + str(issue_month) + '.' + str(issue_year)
-temp_created_date = str(created_day) + '.' + str(created_month)
-temp_issue_date = str(issue_day) + '.' + str(issue_month)
-print(temp_created_date)
-print(temp_issue_date)
+created_date = '25.11.2024'
+issue_date = '01.12.2024'
+print(created_date[0:4])
+print(issue_date[0:4])
