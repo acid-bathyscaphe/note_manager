f8364b36b60d91c76ed16b6688a805038a690fe9
acid-bathyscaphe <Sisterovandrej314@gmail.com>
2024-11-26 18:53:31 +0500

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,12 @@
+username = "Андрей"
+title = "Важные дела"
+content = "Список важных дел на текущую неделю"
+status = "В процессе"
 created_date = '25.11.2024'
 issue_date = '01.12.2024'
-print(created_date[:5])
-print(issue_date[:5])
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date[:5])
+print("Дата истечения заметки:", issue_date[:5])
